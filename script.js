12068a35f0e00db370828301ce4131f7c94d0522
Nour Harrak <n01154202@humber.ca>
2025-04-03 16:46:41 -0400

    added script for form event

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ var keypress = document.getElementById("keyboard")
 keypress.addEventListener('keypress', (e) => {
     document.getElementById("lastkey").textContent = `Your Last Button Press was ${String.fromCharCode(e.charCode)}`;
 });
+
+//Form Event Script
+var form = document.getElementById("form")
+
+form.addEventListener('submit', (e) => {
+    var name = document.getElementById("name")
+    
+    e.preventDefault();
+    alert(`Your name is ${name.value}`)
+});
