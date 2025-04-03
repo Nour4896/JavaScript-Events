d77638acc72b1f7fd0f378352f59f27daa52c929
Nour Harrak <n01154202@humber.ca>
2025-04-03 16:59:39 -0400

    added script for focus and blur events

--- script.js
+++ script.js
@@ -3,26 +3,46 @@
 var hover = document.getElementById("mouse");
 
 hover.addEventListener('mouseenter', () => {
-    hover.textContent = "Hey! I'm being hovered!"
+    hover.textContent = "Hey! I'm being hovered!";
 });
 
 hover.addEventListener('mouseleave', () => {
-    hover.textContent = "Hover Me!"
+    hover.textContent = "Hover Me!";
 });
 
 //Keyboard Event Script
-var keypress = document.getElementById("keyboard")
+var keypress = document.getElementById("keyboard");
 
 keypress.addEventListener('keypress', (e) => {
     document.getElementById("lastkey").textContent = `Your Last Button Press was ${String.fromCharCode(e.charCode)}`;
 });
 
 //Form Event Script
-var form = document.getElementById("form")
+var form = document.getElementById("form");
 
 form.addEventListener('submit', (e) => {
-    var name = document.getElementById("name")
-    
+    var name = document.getElementById("name");
+
     e.preventDefault();
-    alert(`Your name is ${name.value}`)
+    alert(`Your name is ${name.value}`);
+});
+
+//Focus/Blur Events
+var numberCheck = document.getElementById("number")
+
+numberCheck.addEventListener('focus', () => {
+    numberCheck.style.background = "lightblue"
+});
+
+numberCheck.addEventListener('blur', () => {
+    if (numberCheck.value > 10) {
+        numberCheck.style.background = "salmon"
+        document.getElementById("error").textContent = 'This Number is Not Valid'
+    } else if (numberCheck.value === "") {
+        numberCheck.style.background = "salmon"
+        document.getElementById("error").textContent = 'Please Enter a Number'
+    } else {
+        numberCheck.style.background = "lightgreen"
+        document.getElementById("error").textContent = 'This Number Works, Good Choice'
+    }
 });
